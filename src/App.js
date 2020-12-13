58703b4075cf887e4112b4ae35e97f45aac00c8f
William Dias <william.jose.wjd@gmail.com>
2020-12-13 14:15:01 -0300

    definindo algumas estilizaçoes e carregando o filme em destaque da api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import './App.css';
 import tmdb from './tmdb';
 
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 const App = () =>{
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(()=>{
     async function loadAll(){
       const response = await tmdb.getHomeList();
       setMovieList(response);
+
+      // FILME EM DESTAQUE
+      let originals = response.filter((item)=>item.slug === 'originals');
+      let randomChosen = Math.floor(Math.random()*(originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
     loadAll();
   },[])
 
   return(
     <div className="page">
+      {featuredData && (
+        <FeaturedMovie
+          item={featuredData}
+        />
+      )}
+
       <div className="lists">
         {movieList.map((item, key)=>(
           <MovieRow 
---
+++ src/components/FeaturedMovie/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FeaturedMovie.css';
+
+const FeaturedMovie = () =>{
+    return(
+        <h1>Teste</h1>
+    )
+}
+
+export default FeaturedMovie;
