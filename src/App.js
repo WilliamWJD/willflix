b91891c7aed128e24acd7c1445f38ae5cba22784
William Dias <william.jose.wjd@gmail.com>
2020-12-13 13:39:28 -0300

    carregando os dados do tmdb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import tmdb from './tmdb';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import MovieRow from './components/MovieRow';
+
+const App = () =>{
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+    async function loadAll(){
+      const response = await tmdb.getHomeList();
+      setMovieList(response);
+    }
+    loadAll();
+  },[])
+
+  return(
+    <div className="page">
+      <div className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow 
+            key={key} 
+            title={item.title} 
+            items={item.items}
+          />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
